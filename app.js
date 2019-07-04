ed7275dbe41ae63b6f0b490d02a109d5cc08e838
MikeJLeon <michaeljleon@live.com>
2019-07-03 21:20:06 -0700

    Updated port for Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const config = require("./config.json");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOSTNAME = config["heroku"];
 const MongoClient = require("mongodb").MongoClient;
 const uri = config["mConn"];
